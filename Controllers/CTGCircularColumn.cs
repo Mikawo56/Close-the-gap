103e178022d8ac4168d9b82cbd15d709454fda6a
Michael Wauquier <wauquier.michael@outlook.com>
2022-06-24 20:24:02 +0200

    Fix Excel reading

---
+++ Controllers/CTGCircularColumn.cs
@@ -0,0 +1,25 @@
+﻿namespace Close_the_gap.Controllers
+{
+    public enum CTGCircularColumn
+    {
+        ReconditionnerLoadNumber = 0,
+        ReconditionnerTrackingReference = 1,
+        SerialNumber = 2,
+        AssetTag = 3,
+        Type = 5,
+        Brand = 6,
+        Model = 7,
+        CustomSpecifications = 8,
+        CustomDefects = 9,
+        CustomGrade = 10
+    }
+
+    static class CTGCircularColumnMethods
+    {
+
+        public static int GetInt(this CTGCircularColumn column)
+        {
+            return (int)column;
+        }
+    }
+}
--- Controllers/ExcelControler.cs
+++ Controllers/ExcelControler.cs
@@ -1,4 +1,5 @@
-﻿using Close_the_gap.Services;
+﻿using Close_the_gap.Model;
+using Close_the_gap.Services;
 using ExcelDataReader;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -19,10 +20,24 @@ namespace Close_the_gap.Controllers
             _cosmosDbService = cosmosDbService;
         }
 
+        private string GetStringValue(IExcelDataReader reader, CTGCircularColumn column)
+        {
+            var value = reader.GetValue(CTGCircularColumn.AssetTag.GetInt());
+            if(value == null)
+            {
+                return "";
+            }
+            else
+            {
+                return value.ToString();
+            }
+        }
+
         [HttpPost]
         [ActionName("Import")]
         public async Task<ActionResult> ImportAsync(Microsoft.AspNetCore.Http.IFormFile file)
         {
+            var materialList = new List<Material>();
             try
             {
                 using (var ms = new MemoryStream())
@@ -40,7 +55,27 @@ namespace Close_the_gap.Controllers
 
                         // 2. Use the AsDataSet extension method
                         var result = reader.AsDataSet();
-
+                        var iRow = 0;
+                      
+                        while (reader.Read())
+                        {
+                            if(iRow == 0 || iRow == 1 || iRow == 2)
+                            {
+                                iRow++;
+                                continue;
+                            }
+                            var material = new Material()
+                            {
+                                Id = Guid.NewGuid(),
+                                AssetTag = GetStringValue(reader, CTGCircularColumn.AssetTag),
+                                Brand = GetStringValue(reader, CTGCircularColumn.Brand),
+                                Model = GetStringValue(reader, CTGCircularColumn.Model),
+                                Type = GetStringValue(reader, CTGCircularColumn.Type),
+                                SerialNumber = GetStringValue(reader, CTGCircularColumn.SerialNumber),
+                            };
+                            materialList.Add(material);
+                            iRow++;
+                        };
                         // The result of each spreadsheet is in result.Tables
                     }
                 }
@@ -50,7 +85,7 @@ namespace Close_the_gap.Controllers
             }
             
 
-            return Ok();
+            return Ok(materialList);
         }
 
     }
